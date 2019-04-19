cfcfa71cf82dd80ec6f79a6813533b9deef5e68d
Mauricio Martinez <n_othing@hotmail.com>
2019-04-19 11:44:37 -0500

    Removing docker and autoprovision nodejs in ec2 instance

--- main.tf
+++ main.tf
@@ -3,133 +3,101 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "docker" {
-  host = "tcp://${aws_instance.web.public_ip}:2376/"
+#############################################################
+# VPC, subnet, security group and AMI
+#############################################################
+
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  tags {
-    Name = "website-vpc"
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2"
+    ]
   }
-}
 
-resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.default.id}"
-}
+  filter {
+    name = "owner-alias"
 
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.default.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
-}
-
-resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.default.id}"
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-
-  tags {
-    Name = "Public Subnet"
+    values = [
+      "amazon",
+    ]
   }
 }
 
 resource "aws_security_group" "allow_web" {
   name = "allow_http"
-  description = "Allow http and ssh inbound traffic"
-  vpc_id      = "${aws_vpc.default.id}"
+  description = "Allow http and ssh"
+  vpc_id = "${data.aws_vpc.default.id}"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_key_pair" "auth" {
-  key_name   = "${var.key_name}"
-  public_key = "${file(var.public_key_path)}"
+  key_name = "example_key"
+  public_key = "${file("example_keys/public")}"
 }
 
-resource "aws_instance" "web" {
-  ami           = "${var.aws_ami}"
-  instance_type = "${var.aws_type}"
-  count = "1"
-  associate_public_ip_address = "true"
-
-  security_groups = [
-      "${aws_security_group.allow-ssh.name}",
-      "${aws_security_group.allow-docker.name}",
-      "${aws_security_group.allow-all-http-https.name}",
-  ]
-
-  key_name = "${aws_key_pair.auth.id}"
+resource "aws_instance" "ec2" {
+  ami = "${data.aws_ami.amazon_linux.id}"
+  instance_type = "t2.micro"
+  subnet_id = "${element(data.aws_subnet_ids.all.ids, 0)}"
   vpc_security_group_ids = ["${aws_security_group.allow_web.id}"]
-  subnet_id = "${aws_subnet.default.id}"
-
-  tags {
-    Name = "Web"
-    Product = "Docker Nginx and Node"
-  }
+  associate_public_ip_address = true
+  key_name = "${aws_key_pair.auth.name}"
+  tags = "${map("Name", "nodejs-example")}"
 
   provisioner "file" {
-    source      = "./app"
+    source = "./app"
     destination = "/home/ec2-user/app"
     connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file(var.private_key_path)}"
-    }
-  }
-
-  provisioner "file" {
-    source      = "./nginx"
-    destination = "/home/ec2-user/nginx"
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file(var.private_key_path)}"
-    }
-  }
-
-  provisioner "file" {
-    source      = "docker-compose.yml"
-    destination = "/home/ec2-user/docker-compose.yml"
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file(var.private_key_path)}"
+      type = "ssh"
+      user = "ec2-user"
+      private_key = "${file("example_keys/private")}"
     }
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum -y update",
-      "sudo curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > docker-compose",
-      "sudo mv docker-compose /usr/local/bin/docker-compose",
-      "sudo chmod +x /usr/local/bin/docker-compose",
-      "sudo /usr/local/bin/docker-compose up -d --scale web=2",
+      "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash",
+      ". ~/.nvm/nvm.sh",
+      "nvm install 8",
+      "node /home/ec2-user/app/index.js"
     ],
+
     connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file(var.private_key_path)}"
+      type = "ssh"
+      user = "ec2-user"
+      private_key = "${file("example_keys/private")}"
     }
   }
 }
+
+output "public_ip" {
+  value = "${aws_instance.ec2.public_ip}"
+}
+
